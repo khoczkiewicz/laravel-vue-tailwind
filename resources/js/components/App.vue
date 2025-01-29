3ee736a54371270c59ba9c2d863b8172a4c88684
K. Hoczkiewicz <32704808+khoczkiewicz@users.noreply.github.com>
2025-01-29 18:30:11 +0100

    Initial project
    An example of Laravel 11 + Vue 3.5 + Tailwind 4.0 project with installation and configuration steps

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="p-6 bg-gray-200 text-center text-xl font-bold">
+        Welcome to Laravel 11 + Vue 3.5 + Tailwind 4.0!
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style scoped></style>
