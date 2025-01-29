0dd74e1c1f1831969b3991ed7df4a4eaa5b4dfb8
K. Hoczkiewicz <32704808+khoczkiewicz@users.noreply.github.com>
2025-01-30 00:11:13 +0100

    AppHeader provided as component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="p-6 bg-gray-200 text-center text-xl font-bold">
-        Welcome to Laravel 11 + Vue 3.5 + Tailwind 4.0!
-    </div>
+    <AppHeader />
 </template>
+
+<script setup>
+import AppHeader from './AppHeader.vue';
+</script>
 
 <script>
 export default {
